f0420544e20c3b7c51c9050f4499e43c95c41ef0
SergioCampos99 <s.camposlacueva@gmail.com>
2021-05-19 18:57:44 +0200

    Register completed

---
+++ newsapp4/app/src/main/java/com/example/newsapp4/Connection/ConnectionClass.java
@@ -0,0 +1,9 @@
+package com.example.newsapp4.Connection;
+
+public class ConnectionClass {
+    public static String ip = "85.251.26.50";
+    public static String un = "scampos";
+    public static String pass = "12345";
+    public static String db = "AppNews";
+    public static String port = "8887";
+}
--- newsapp4/app/src/main/java/com/example/newsapp4/MainActivity.java
+++ newsapp4/app/src/main/java/com/example/newsapp4/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.newsapp4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.VideoView;
+
+import com.example.newsapp4.Connection.ConnectionClass;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +29,35 @@ public class MainActivity extends AppCompatActivity {
     VideoView videoBack;
     EditText pass, email, username;
     Intent intencion;
+    Connection con;
+    Statement stmt;
+    //TextView
+    TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         videoBack = (VideoView) findViewById(R.id.vvBack);
+
+        //btnRegistro
         botonRegistro = (Button) findViewById(R.id.btn_registro);
+
+        //TextView
+        status = (TextView) findViewById(R.id.tvRegister);
+
+        //editText
         email = (EditText) findViewById(R.id.etEmail);
+        pass = (EditText) findViewById(R.id.etPass);
+        username = (EditText) findViewById(R.id.etUsername);
+
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    new MainActivity.registerUser().execute("");
+            }
+        });
+
         botonInicio = (Button) findViewById(R.id.btn_inicioSesion);
 
         String path ="android.resource://com.example.newsapp4/"+R.raw.mix;
@@ -57,6 +90,67 @@ public class MainActivity extends AppCompatActivity {
         });
         */
     }
+
+    public class registerUser extends AsyncTask<String, String, String>{
+
+        String z = "";
+        Boolean isSuccess = false;
+
+        @Override
+        protected void onPreExecute() {
+            status.setText("Sending data to Database");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            status.setText("Registration Succesfull");
+            username.setText("");
+            email.setText("");
+            pass.setText("");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                con = connectionClass(ConnectionClass.ip.toString(), ConnectionClass.port.toString(),ConnectionClass.db.toString(),ConnectionClass.un.toString(),ConnectionClass.pass.toString());
+
+                if(con == null){
+                    z = "Check your Internet Connection";
+                }else{
+                    String sql = "INSERT INTO users (Username, Pass, Email) VALUES ('"+ username.getText()+"', '"+pass.getText()+"','"+email.getText()+"')";
+                    stmt = con.createStatement();
+                    stmt.executeUpdate(sql);
+                }
+            }catch(Exception e){
+                    isSuccess = false;
+                    z = e.getMessage();
+            }
+
+            return z;
+        }
+    }
+
+    @SuppressLint("NewApi")
+    public Connection connectionClass(String server, String port, String database, String username, String pass){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String connectionURL = null;
+        try{
+                Class.forName("net.sourceforge.jtds.jdbc.Driver");
+                connectionURL= "jdbc:jtds:sqlserver://"+ server + ":" +port+ ";"+ "databaseName="+ database +";user="+username+";password="+pass+";";
+                connection = DriverManager.getConnection(connectionURL);
+                System.out.println(connectionURL);
+        }catch (Exception e){
+            Log.e("SQL Connection Error : ", e.getMessage());
+            System.out.println(connectionURL);
+        }
+
+        return connection;
+
+    };
+
+
     protected void onResume(){
         videoBack.resume();
         super.onResume();
@@ -70,10 +164,11 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    public void PasarDatos(View vista){
+
+    /*public void PasarDatos(View vista){
         intencion = new Intent(this, menuContent.class);
         startActivity(intencion);
-    }
+    }*/
 
 
 }
